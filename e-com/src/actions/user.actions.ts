5b3b130454921d4ce39b70f354cf4c87c569d466
pranith kumar pendota <145289341+pranithkumar01@users.noreply.github.com>
2025-03-11 00:08:48 +0530

    Update user.actions.ts interfac

--- e-com/src/actions/user.actions.ts
+++ e-com/src/actions/user.actions.ts
@@ -1,17 +1,39 @@
-"use server"
+// "use server"
 
-import User from "@/modals/user.modal"
+// import User from "@/modals/user.modal"
 
-import {connect} from "@/db"
+// import {connect} from "@/db"
 
-export async function createUser(user:any){
+// export async function createUser(user:any){
 
-    try{
-        await connect()
-        const newUser= await User.create(user)
-        return JSON.parse(JSON.stringify(newUser))
+//     try{
+//         await connect()
+//         const newUser= await User.create(user)
+//         return JSON.parse(JSON.stringify(newUser))
 
-    }catch(err){
-        console.log(err)
+//     }catch(err){
+//         console.log(err)
+//     }
+// }
+
+"use server";
+
+import User from "@/modals/user.modal";
+import { connect } from "@/db";
+
+interface IUser {
+    // Define the structure of the user object here
+    name: string;
+    email: string;
+    // Add other properties as needed
+}
+
+export async function createUser(user: IUser) {
+    try {
+        await connect();
+        const newUser = await User.create(user);
+        return JSON.parse(JSON.stringify(newUser));
+    } catch (err) {
+        console.log(err);
     }
 }
