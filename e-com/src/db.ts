5466af864ca68165b40a3d761ae6d73bd1b87ff8
pranith kumar pendota <145289341+pranithkumar01@users.noreply.github.com>
2025-03-12 00:27:46 +0530

    Update db.ts

--- e-com/src/db.ts
+++ e-com/src/db.ts
@@ -4,35 +4,35 @@ import mongoose,{ Mongoose } from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI!;
 
-interface MongooseConn{
-    conn: Mongoose | null;
-    promise: Promise<Mongoose> | null;
-}
+// interface MongooseConn{
+//     conn: Mongoose | null;
+//     promise: Promise<Mongoose> | null;
+// }
 
-let cashed: MongooseConn=(global as any).mongoose;
+// let cashed: MongooseConn=(global as any).mongoose;
 
-if(!cashed){
-    cashed=(global as any).mongoose={
-        conn:null,
-        promise:null,
-    }
-}
+// if(!cashed){
+//     cashed=(global as any).mongoose={
+//         conn:null,
+//         promise:null,
+//     }
+// }
 
-export const connect =async()=>{
+// export const connect =async()=>{
 
-    if(cashed.conn){
-        return cashed.conn;
-    }
-    cashed.promise=cashed.promise || mongoose.connect(MONGODB_URI,{
+//     if(cashed.conn){
+//         return cashed.conn;
+//     }
+//     cashed.promise=cashed.promise || mongoose.connect(MONGODB_URI,{
 
-        dbName:"e-com",
-        bufferCommands:false,
-        connectTimeoutMS:3000,
-    })
-    cashed.conn=await cashed.promise;
+//         dbName:"e-com",
+//         bufferCommands:false,
+//         connectTimeoutMS:3000,
+//     })
+//     cashed.conn=await cashed.promise;
 
-    return cashed.conn;
+//     return cashed.conn;
 
-}
+// }
 //praneethkumarpendota21
 //GO5c41PWWIfoGWLo
