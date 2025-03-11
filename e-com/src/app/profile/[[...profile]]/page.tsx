b956acd50261505b017f16026b03759ac93c77db
pranith kumar pendota <145289341+pranithkumar01@users.noreply.github.com>
2025-03-11 23:37:07 +0530

    Update page.tsx

--- e-com/src/app/profile/[[...profile]]/page.tsx
+++ e-com/src/app/profile/[[...profile]]/page.tsx
@@ -25,8 +25,35 @@
 // export default Profile
 
 
+// import React from 'react'
+// import { useRouter } from 'next/router'
+// import { UserProfile } from '@clerk/nextjs'
+// import { auth, currentUser } from '@clerk/nextjs/server'
+
+// async function Profile() {
+//   const router = useRouter()
+//   const userId = await auth()
+//   const isAuth = !!userId
+//   const user = await currentUser()
+
+//   React.useEffect(() => {
+//     if (!isAuth) {
+//       router.push('/')
+//     }
+//   }, [isAuth, router])
+
+//   return (
+//     <div className='flex flex-col items-center justify-center h-full mt-8'>
+//       <h1 className='text-2xl'>{user?.username}</h1>
+//       <UserProfile />
+//     </div>
+//   )
+// }
+
+// export default Profile
+
 import React from 'react'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 import { UserProfile } from '@clerk/nextjs'
 import { auth, currentUser } from '@clerk/nextjs/server'
 
@@ -51,3 +78,4 @@ async function Profile() {
 }
 
 export default Profile
+
