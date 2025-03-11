4bdaf20aabbdbdda2e55a20fc05f30894cae5bf6
pranith kumar pendota <145289341+pranithkumar01@users.noreply.github.com>
2025-03-11 21:37:35 +0530

    Update user.modal.ts

--- e-com/src/modals/user.modal.ts
+++ e-com/src/modals/user.modal.ts
@@ -1,5 +1,5 @@
-import { Schema,model,models } from "mongoose";
-import { unique } from "next/dist/build/utils";
+import { Schema,model } from "mongoose";
+// import { unique } from "next/dist/build/utils";
 
 const userSchema = new Schema({
     clerkId:{
