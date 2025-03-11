0bdd089e3b003a555dbd8ebcf4a86241d1eb968f
pranith kumar pendota <145289341+pranithkumar01@users.noreply.github.com>
2025-03-11 23:51:02 +0530

    Update page.tsx

--- e-com/src/app/profile/[[...profile]]/page.tsx
+++ e-com/src/app/profile/[[...profile]]/page.tsx
@@ -52,32 +52,58 @@
 
 // export default Profile
 
-"use client"
+// "use client"
 
-import React from 'react'
-import { useRouter } from 'next/navigation'
-import { UserProfile } from '@clerk/nextjs'
+// import React from 'react'
+// import { useRouter } from 'next/navigation'
+// import { UserProfile } from '@clerk/nextjs'
+// import { auth, currentUser } from '@clerk/nextjs/server'
+
+// async function Profile() {
+//   const router = useRouter()
+//   const userId = await auth()
+//   const isAuth = !!userId
+//   const user = await currentUser()
+
+//   React.useEffect(() => {
+//     if (!isAuth) {
+//       router.push('/')
+//     }
+//   }, [isAuth, router])
+
+//   return (
+//     <div className='flex flex-col items-center justify-center h-full mt-8'>
+//       <h1 className='text-2xl'>{user?.username}</h1>
+//       <UserProfile />
+//     </div>
+//   )
+// }
+
+// export default Profile
+
 import { auth, currentUser } from '@clerk/nextjs/server'
 
-async function Profile() {
-  const router = useRouter()
+export async function getServerSideProps() {
   const userId = await auth()
   const isAuth = !!userId
   const user = await currentUser()
 
-  React.useEffect(() => {
-    if (!isAuth) {
-      router.push('/')
+  if (!isAuth) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
     }
-  }, [isAuth, router])
+  }
 
-  return (
-    <div className='flex flex-col items-center justify-center h-full mt-8'>
-      <h1 className='text-2xl'>{user?.username}</h1>
-      <UserProfile />
-    </div>
-  )
+  return {
+    props: {
+      user,
+    },
+  }
 }
 
-export default Profile
+
+
 
