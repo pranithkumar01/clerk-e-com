08640c39fcf082341c3fb4cf831cf11153939e5c
pranith kumar pendota <145289341+pranithkumar01@users.noreply.github.com>
2025-03-11 21:30:41 +0530

    Update Navbar.tsx

--- e-com/src/app/components/Navbar.tsx
+++ e-com/src/app/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { UserButton } from '@clerk/nextjs'
-import { auth, currentUser } from '@clerk/nextjs/server'
+import {  currentUser } from '@clerk/nextjs/server'
 import Link from 'next/link'
 import React from 'react'
 
